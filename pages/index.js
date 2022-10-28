c6479b8bda7c2701f81f94f4223edbda4a3248dc
Natalya Shymkova <Natalia.Matiishena@yandex.ru>
2022-10-28 12:16:24 +0300

    pop-up add

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 //pop-up
-const openEdit = document.querySelector(".profile__edit");
+const btnEdit = document.querySelector(".profile__edit");
 const page = document.querySelector(".page");
 const popupTemplate = document.querySelector("#pop-up").content;
 const popupElement = popupTemplate.querySelector(".pop-up").cloneNode(true);
+const btnClosed = popupElement.querySelector(".pop-up__closed");
+const formElement = popupElement.querySelector("#formId");
+const nameInput = formElement.querySelector(".form__text_name");
+const historyInput = formElement.querySelector(".form__text_history");
+const btnAdd = document.querySelector(".profile__add");
 
-openEdit.addEventListener("click", function () {
+btnEdit.addEventListener("click", openEdit);
+btnClosed.addEventListener("click", popupClosed);
+formElement.addEventListener("submit", formSubmitHandler);
+btnAdd.addEventListener("click", openAdd);
+
+function openPopup() {
   popupElement.classList.add("pop-up_active");
   page.prepend(popupElement);
-});
+}
 
-const btnClosed = popupElement.querySelector(".pop-up__closed");
-btnClosed.addEventListener("click", popupClosed);
 function popupClosed() {
   popupElement.classList.remove("pop-up_active");
   popupElement.remove();
@@ -18,12 +26,7 @@ function popupClosed() {
   historyInput.value = "";
 }
 
-const formElement = popupElement.querySelector("#formId");
-const nameInput = formElement.querySelector(".form__text_name");
-const historyInput = formElement.querySelector(".form__text_history");
-formElement.addEventListener("submit", formSubmitHandler);
 function formSubmitHandler(evt) {
-  console.log("test");
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // заполнили
   const profeleName = document.querySelector(".profile__name");
@@ -33,4 +36,27 @@ function formSubmitHandler(evt) {
   profileOccupation.textContent = historyInput.value;
   //окошко закрыли
   popupClosed();
+}
+
+function openEdit() {
+  popupElement.querySelector(".pop-up__title").textContent =
+    "Редактировать профиль";
+  popupElement.querySelector(".form__text_name").placeholder = "Ваше имя";
+  popupElement.querySelector(".form__text_name").name = "profile_name";
+  popupElement.querySelector(".form__text_history").placeholder =
+    "Расскажите о себе";
+  popupElement.querySelector(".form__text_history").name = "history";
+  popupElement.querySelector(".form__save").textContent = "Сохранить";
+  openPopup();
+}
+
+function openAdd() {
+  popupElement.querySelector(".pop-up__title").textContent = "Новое место";
+  popupElement.querySelector(".form__text_name").placeholder = "Название";
+  popupElement.querySelector(".form__text_name").name = "img_name";
+  popupElement.querySelector(".form__text_history").placeholder =
+    "Ссылка на картинку";
+  popupElement.querySelector(".form__text_history").name = "crs_img";
+  popupElement.querySelector(".form__save").textContent = "Создать";
+  openPopup();
 }
