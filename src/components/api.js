6510b9c411dde686cc565c8f2a29117077e9c5fb
Natalya Shymkova <Natalia.Matiishena@yandex.ru>
2022-12-16 00:28:36 +0300

    add api

---
+++ src/components/api.js
@@ -0,0 +1,55 @@
+const config = {
+  baseUrl: "https://nomoreparties.co/v1/cohort-55",
+  headers: {
+    authorization: "8fb99c7c-392b-4433-90e5-81e0ad818670",
+    "Content-Type": "application/json",
+  },
+};
+
+export function getUser() {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: {
+      authorization: config.headers.authorization,
+    },
+  });
+}
+
+export function updateProfile(name, description) {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: name.value,
+      about: description.value,
+    }),
+  });
+}
+
+export function updateImgProfile(link) {
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar: link.value,
+    }),
+  });
+}
+
+export function getGalery() {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: {
+      authorization: config.headers.authorization,
+    },
+  });
+}
+
+export function createCardApi(name, src) {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: name.value,
+      link: src.value,
+    }),
+  });
+}
